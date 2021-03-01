3a551c9b6979a3725213488719a2cd5a07a1cc0f
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-03-01 23:49:12 +0700

    Filter search

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
-    <div class="loading" v-if="loading">Loading.....</div>
-    <div class="list" v-else>
-      <ul>
-        <li v-for="(surah, index) in surah" :key="index">
-          <router-link v-bind:to="'/surah/' + surah.number">{{
-            surah.name.translation.id
-          }}</router-link>
-        </li>
-      </ul>
+    <div class="container">
+      <input type="text" v-model="search" placeholder="cari surat" />
+      <div class="loading" v-if="loading">
+        <div class="spinner-grow" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+      </div>
+      <div v-else>
+        <div class="card-group">
+          <div class="row">
+            <div
+              class="col-4"
+              v-for="(surah, index) in filterSurah"
+              :key="index"
+            >
+              <router-link v-bind:to="'/surah/' + surah.number">
+                <div class="card mb-5">
+                  <div class="card-body">
+                    <div class="card-title">
+                      {{ surah.name.transliteration.id }}
+                    </div>
+                  </div>
+                </div>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -24,10 +43,11 @@ export default {
   data() {
     return {
       loading: true,
-      surah: null
+      surah: {},
+      search: ""
     };
   },
-  mounted() {
+  created() {
     axios
       .get(url)
       .then(response => {
@@ -37,6 +57,28 @@ export default {
         console.log(error);
       })
       .finally(() => (this.loading = false));
+  },
+  computed: {
+    filterSurah: function() {
+      return this.surah.filter(surah => {
+        return surah.name.transliteration.id.match(this.search);
+      });
+    }
   }
 };
 </script>
+<style scope>
+.card {
+  max-width: 400px;
+  width: 300px;
+}
+.loading {
+  width: 100%;
+  max-width: 700px;
+  margin: auto;
+  align-items: center;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+}
+</style>
